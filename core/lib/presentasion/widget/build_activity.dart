7734e242121fa14639eb48376767af3dee7f974b
laurensiusyuda <yudalaurensius87@gmail.com>
2023-03-15 23:48:02 +0700

    fix update time

--- core/lib/presentasion/widget/build_activity.dart
+++ core/lib/presentasion/widget/build_activity.dart
@@ -1,4 +1,4 @@
-import 'package:core/helper/datetimehelper.dart';
+import 'package:core/presentasion/widget/flexible_time.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -74,64 +74,6 @@ class _BuildDashboardState extends State<BuildDashboard> {
             ),
           ),
         ],
-      ),
-    );
-  }
-}
-
-Flexible timeNow() {
-  var time = DatetimeHelper.dateTimeScheduled();
-  if (time.hour >= 6 && time.hour < 10) {
-    return Expanded(
-      child: Text(
-        'Selamat Pagi',
-        style: GoogleFonts.lato(
-          textStyle: const TextStyle(
-            fontSize: 15,
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-      ),
-    );
-  } else if (time.hour >= 10 && time.hour < 15) {
-    return Expanded(
-      child: Text(
-        'Selamat Siang',
-        style: GoogleFonts.lato(
-          textStyle: const TextStyle(
-            fontSize: 15,
-            color: Colors.black,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-      ),
-    );
-  } else if (time.hour >= 15 && time.hour < 18) {
-    return Expanded(
-      child: Text(
-        'Selamat Sore ',
-        style: GoogleFonts.lato(
-          textStyle: const TextStyle(
-            fontSize: 15,
-            color: Colors.black,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-      ),
-    );
-  } else {
-    return Flexible(
-      child: Text(
-        'Selamat Malam selamat datang diaplikasi Udangku',
-        textAlign: TextAlign.center,
-        style: GoogleFonts.lato(
-          textStyle: const TextStyle(
-            fontSize: 15,
-            color: Colors.black,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
       ),
     );
   }
