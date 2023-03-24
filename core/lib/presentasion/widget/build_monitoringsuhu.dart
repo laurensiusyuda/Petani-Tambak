c04d79b2501c68271238b28ccac6b6441aeaff93
laurensiusyuda <yudalaurensius87@gmail.com>
2023-03-24 23:34:32 +0700

    build monitoring

--- core/lib/presentasion/widget/build_monitoringsuhu.dart
+++ core/lib/presentasion/widget/build_monitoringsuhu.dart
@@ -1,5 +1,3 @@
-import 'package:core/helper/datetimehelper.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:utils/utils.dart';
@@ -9,6 +7,52 @@ class BuildMonitoringSuhu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView();
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, 'routeName');
+      },
+      child: Card(
+        color: kSecondaryColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(25.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: kMainColor,
+                  border: Border.all(color: kGreenColor, width: 2.0),
+                ),
+                child: Column(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(10.0),
+                      decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: kSecondaryColor,
+                          boxShadow: [
+                            BoxShadow(
+                                color: kSecondaryColor,
+                                blurRadius: 10.0,
+                                spreadRadius: 3.0)
+                          ]),
+                      child: Image.asset(
+                        'asset/thermometer.png',
+                        width: 50,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
